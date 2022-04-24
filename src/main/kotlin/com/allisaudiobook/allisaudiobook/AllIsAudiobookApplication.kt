9d2e8bf6e472bab2184ed571e8c672c5c725b0a9
ElvisNogueira <elvis.nogueira150@gmail.com>
2022-04-24 14:01:44 -0300

    Initial scaffold

---
+++ src/main/kotlin/com/allisaudiobook/allisaudiobook/AllIsAudiobookApplication.kt
@@ -0,0 +1,11 @@
+package com.allisaudiobook.allisaudiobook
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AllIsAudiobookApplication
+
+fun main(args: Array<String>) {
+	runApplication<AllIsAudiobookApplication>(*args)
+}
---
+++ src/test/kotlin/com/allisaudiobook/allisaudiobook/AllIsAudiobookApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.allisaudiobook.allisaudiobook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AllIsAudiobookApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
